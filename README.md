f82441289274eb862892b700723ad02dd352b43f
Gerellz <gerellz93@gmail.com>
2026-04-30 01:59:57 -0500

    Updating files to integrate with TMDB API

--- README.md
+++ README.md
@@ -6,5 +6,6 @@ A command-line driven movie tracker so I can keep track of movies that I've like
 
 Ability to add and remove movies from JSON list
 Search for any movie on list by title to retrieve release date
+Show all movies on the list by title and release date
 
 #
--- pythonLearning3.py
+++ pythonLearning3.py
@@ -1,6 +1,19 @@
 import json
+import os
 
 movies = []
+
+def loadMovies():
+    global movies
+    try:
+        with open("movies.json", "r") as f:
+            movies = json.load(f)
+    except FileNotFoundError:
+        movies = []
+
+def saveMovies():
+    with open("movies.json", "w") as f:
+        json.dump(movies, f)
 
 def addMovie(title, release_date):
     moviedict = {"title" : title, "release_date" : release_date}
@@ -24,34 +37,34 @@ def getMovie(title):
             return 
     print(f"Sorry, {title} was not found!")
 
-def saveMovies():
-    with open("movies.json", "w") as f:
-        json.dump(movies, f)
+def listMovie():
+    for movie in movies:
+        print(f"{movie['title']} : {movie['release_date']}")
 
-def loadMovies():
-    global movies
-    try:
-        with open("movies.json", "r") as f:
-            movies = json.load(f)
-    except FileNotFoundError:
-        movies = []
-
+def clearScreen():
+    os.system('cls' if os.name == 'nt' else 'clear')
 
 while True:
     loadMovies()
+    clearScreen()
     crudMenu = input("What would you like to do?" \
     "\n1. Add a movie\n2. Remove a movie" \
-    "\n3. Get a movie\n4. Quit\n")
+    "\n3. Get a movie\n4. List all movies\nQ. Quit\n")
     if crudMenu == "1":
         addMovieTitle = input("Name of movie to add to database: ")
         addMovieReleaseDate = input(f"Release date of {addMovieTitle}: ")
         addMovie(addMovieTitle, addMovieReleaseDate)
+        input("")
     elif crudMenu == "2":
         removeMovieTitle = input("Name of movie to remove: ")
         removeMovie(removeMovieTitle)
+        input("")
     elif crudMenu == "3":
         getMovieTitle = input("Name of movie to find in database: ")
         getMovie(getMovieTitle)
+        input("")
     elif crudMenu == "4":
+        listMovie()
+        input("")
+    elif crudMenu == "Q" or crudMenu == "q":
         break
-    

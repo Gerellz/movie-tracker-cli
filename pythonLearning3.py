bb6d2ce1311b38e604af9a2b814e0617a980c4d0
Gerellz <gerellz93@gmail.com>
2026-05-01 00:54:22 -0500

    Updated some of the searching, adding, and removing logic.

--- pythonLearning3.py
+++ pythonLearning3.py
@@ -1,5 +1,6 @@
 import json
 import os
+import tmdb
 
 movies = []
 
@@ -13,29 +14,38 @@ def loadMovies():
 
 def saveMovies():
     with open("movies.json", "w") as f:
-        json.dump(movies, f)
+        json.dump(movies, f, indent=4)
 
-def addMovie(title, release_date):
-    moviedict = {"title" : title, "release_date" : release_date}
+def addMovie(title, release_date, description):
+    moviedict = {"title" : title, "release_date" : release_date, "description" : description}
+    for movie in movies:
+        if movie["title"].lower() == title.lower():
+            print(f"{movie['title']} is already in the database!")
+            return
     movies.append(moviedict)
     print(f"Successfully added {moviedict['title']}.")
     saveMovies()
 
 def removeMovie(title):
     for movie in movies:
-        if movie["title"] == title:
+        if movie["title"].lower() == title.lower():
             print(f"Removing {movie['title']} from list...")
             movies.remove(movie)
             saveMovies()
             return
+        else:
+            print("Sorry, that movie was not found. Maybe you misspelled it?")
         
 def getMovie(title):
+    found = False
     for movie in movies:
-        if movie["title"] == title:
+        if title.lower() in (movie["title"]).lower():
             print(f"Title: {movie['title']}")
             print(f"Release Date: {movie['release_date']}")
-            return 
-    print(f"Sorry, {title} was not found!")
+            print(f"Description: {movie['description']}")
+            found = True
+    if not found:
+        print(f"Sorry, {title} was not found!")
 
 def listMovie():
     for movie in movies:
@@ -51,20 +61,23 @@ while True:
     "\n1. Add a movie\n2. Remove a movie" \
     "\n3. Get a movie\n4. List all movies\nQ. Quit\n")
     if crudMenu == "1":
-        addMovieTitle = input("Name of movie to add to database: ")
-        addMovieReleaseDate = input(f"Release date of {addMovieTitle}: ")
-        addMovie(addMovieTitle, addMovieReleaseDate)
-        input("")
+        searchMovieTitle = input("What are you looking for? ")
+        returnedMovie = tmdb.searchMovie(searchMovieTitle)
+        if returnedMovie is not None:
+            addMovie(returnedMovie["title"], returnedMovie["release_date"], returnedMovie["overview"])
+            input("Can't wait to see it! Press any key to continue.")
     elif crudMenu == "2":
+        listMovie()
         removeMovieTitle = input("Name of movie to remove: ")
         removeMovie(removeMovieTitle)
-        input("")
+        input("Yeah, I never liked that one either. Press any key to continue.")
     elif crudMenu == "3":
         getMovieTitle = input("Name of movie to find in database: ")
         getMovie(getMovieTitle)
-        input("")
+        input("Oh, that's a good one! Press any key to continue.")
     elif crudMenu == "4":
         listMovie()
-        input("")
+        input("Find anything you like? Press any key to continue.")
     elif crudMenu == "Q" or crudMenu == "q":
+        print("Goodbye.")
         break
--- tmdb.py
+++ tmdb.py
@@ -2,17 +2,38 @@ import requests
 import os
 from dotenv import load_dotenv
 
+# reads the env file and loads contents for os.getenv
 load_dotenv()
 
+# stores the api key from the env file into a usable variable
 API_KEY = os.getenv("TMDB_API_KEY")
 
+# method to query the database and return a list of movies that the user might be referencing
 def searchMovie(query):
-    url = f"https://api.themoviedb.org/3/search/movie"
+    # takes the url and appends the api key and the query subject
+    url = "https://api.themoviedb.org/3/search/movie"
     params = {
         "api_key": API_KEY,
         "query": query
     }
-    response = requests.get(url, params=params)
+    # sends a get request to the url with the parameters
+    try:
+        response = requests.get(url, params=params)
+    except requests.exceptions.ConnectionError:
+        print("Uh-oh. Looks like you're not connected!")
+        return None
+    # receives a response and stores that json into a variable, data
     data = response.json()
+    # loops through the data and returns an indexed page of results related to the query
     for i, movie in enumerate(data["results"][:5]):
         print(f"{i+1}. {movie['title']} - {movie['release_date']}")
+    try:
+        choice = int(input("Select a movie from the list by number: "))
+        if choice < 1 or choice > 5:
+            print("Error. Please select a movie from list.")
+            return None
+    except ValueError:
+        print("Error. Please enter a valid selection.")
+        return None
+    selected = data["results"][choice - 1]
+    return selected
